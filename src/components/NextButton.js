841d3ab9d5ddb9003e16eaad130b85fe43ac3ad7
Ojo <oluwatobiojo2911@gmail.com>
2024-01-27 10:14:50 +0100

    added general knowledge questions and auto finish quiz

--- src/components/NextButton.js
+++ src/components/NextButton.js
@@ -1,9 +1,16 @@
+import { useEffect } from "react";
 import { useQuiz } from "../context/QuizContext";
 
 function NextButton() {
   const { index, diffQuestions, dispatch } = useQuiz();
   const numQuestions = diffQuestions.length;
   // const lastPage = index < numQuestions - 1;
+  useEffect(
+    function () {
+      if (index === numQuestions - 1) dispatch({ type: "finished" });
+    },
+    [index, numQuestions, dispatch]
+  );
   if (index < numQuestions - 1)
     return (
       <button
@@ -13,15 +20,15 @@ function NextButton() {
         Next
       </button>
     );
-  else
-    return (
-      <button
-        className="btn btn-ui"
-        onClick={() => dispatch({ type: "finished" })}
-      >
-        Finish
-      </button>
-    );
+  // else
+  //   return (
+  //     <button
+  //       className="btn btn-ui"
+  //       onClick={() => dispatch({ type: "finished" })}
+  //     >
+  //       Finish
+  //     </button>
+  //   );
 }
 
 export default NextButton;
