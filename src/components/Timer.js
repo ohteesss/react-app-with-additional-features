581c17e0991f7a2ca22014d670293ef29e11a936
Ojo <oluwatobiojo2911@gmail.com>
2024-09-10 17:10:17 +0100

    Remove unused useState import in Timer component

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useQuiz } from "../context/QuizContext";
 function Timer() {
   const { dispatch, secondsRemaining } = useQuiz();
